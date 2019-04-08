70202eef05c673189684a1d1d57b350c44b1d0d8
7kn <tknoops@gmail.com>
2019-04-08 16:54:50 +0200

    added shortest word kata

---
+++ ryu7/shortest_word.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# My anwser:
+def find_short(s):
+    word_list = list(s.split())
+    word_len_dict = {}
+    
+    for word in word_list:
+      word_len_dict[word] = len(word)
+      print(word_len_dict)
+    
+    smallest_key = min(word_len_dict, key= lambda x: word_len_dict.get(x))
+    l = word_len_dict[smallest_key]
+    print(l)
+    
+    return l # l: shortest word length
+
+# Best anwser:
+#def find_short(s):
+#    return min(len(x) for x in s.split())
